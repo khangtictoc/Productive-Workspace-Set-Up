fe706429b20f15d832d42a4fd5e130d3c925054b
khangtictoc <tranhoangkhang09122001@gmail.com>
2024-08-20 00:17:16 +0700

    Fix bugs omp and add ide settings for vscode

--- linux/terminal/ui/oh-my-posh/install.sh
+++ linux/terminal/ui/oh-my-posh/install.sh
@@ -5,5 +5,7 @@ curl -s https://ohmyposh.dev/install.sh | bash -s
 # oh-my-posh font install
 # oh-my-posh font install <font-name>
 
-oh-my-posh init fish --config /home/virus/.cache/oh-my-posh/themes/cloud-native-azure.omp.json > ~/.oh-my-posh-init && source ~/.oh-my-posh-init
+set THEME "cloud-native-azure"
+set APPLIED_SHELL "fish"
+oh-my-posh init $APPLIED_SHELL --config ~/.cache/oh-my-posh/themes/$THEME.omp.json > ~/.oh-my-posh-init && source ~/.oh-my-posh-init
 echo -n "[ -f ~/.oh-my-posh-init ] && source ~/.oh-my-posh-init" | tee -a ~/.config/fish/config.fish
--- windows/terminal/plugins/fzf/install.sh
+++
@@ -1,5 +0,0 @@
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-cat <<EOF > ~/.fzf/install
-y
-y
-EOF
