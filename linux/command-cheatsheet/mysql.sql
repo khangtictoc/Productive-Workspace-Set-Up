6a9cbb27ecb0c0576c834683adf3b7acda523825
Khang Hoang Tran <tranhoangkhang09112001@gmail.com>
2024-10-03 17:16:12 +0700

    Refactor folder structure & add k8s cheatsheet for viewing some custom columns

--- linux/command-cheatsheet/mysql.sql
+++ linux/command-cheatsheet/mysql.sql
@@ -9,3 +9,10 @@ SELECT table_schema "DB Name",
         ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) "DB Size in MB" 
 FROM information_schema.tables 
 GROUP BY table_schema; 
+
+SHOW VARIABLES WHERE Variable_name = 'innodb_buffer_pool_size'
+OR Variable_name = 'innodb_log_file_size'
+OR Variable_name = 'max_allowed_packet'
+OR Variable_name = 'innodb_flush_log_at_trx_commit'
+OR Variable_name = 'innodb_thread_concurrency'
+OR Variable_name = 'innodb_doublewrite';
