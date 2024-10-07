fdf4b3fae34a232a5be1e6a56fd2af9a28f3b809
Khang Hoang Tran <khang.tran@mercatus.com>
2024-10-07 22:50:05 +0700

    Modify SQL commands & packages for new VM Debug

---
+++ linux/command-cheatsheet/az-cli/blob/commands.sh
@@ -0,0 +1,2 @@
+az storage blob upload     --account-name bigymigrationbackup     --container-name matomo      --name all_databases_prod.sql.zip     --file all_databases_prod.sql.zip
+az storage blob download     --account-name bigymigrationbackup     --container-name matomo      --name all_databases_prod.sql.zip     --file all_databases_prod.sql.zip
