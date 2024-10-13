305e5a72781fadebf3418fd0bc891642d160024b
khangtictoc <tranhoangkhang09112001@gmail.com>
2024-10-13 21:02:52 +0700

    Add automation config oh-my-posh for zsh and fish

--- linux/terminal/ui/oh-my-posh/configure-zsh.sh
+++ linux/terminal/ui/oh-my-posh/configure-zsh.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/fish
+#! /bin/bash
 
 ####### Run for FISH only ######
 # Recommend theme
@@ -9,4 +9,9 @@ THEME="easy-term"
 eval "\$(oh-my-posh init zsh --config ~/.cache/oh-my-posh/themes/\$THEME.omp.json)"
 EOF
 
-echo "exec zsh" | tee -a ~/.bashrc
+if grep -Fxq "[ -f ~/.oh-my-posh-init ] && source ~/.oh-my-posh-init" ~/.bashrc; then
+    echo "Bash file config has already source the ~/.oh-my-posh-init at bash startup. NO MODIFY !!!"
+else
+    echo "exec zsh" | tee -a ~/.bashrc
+    echo "\nFish file config will source the ~/.oh-my-posh-init at fish startup". MODIFY SUCCESSFULLY !!!
+fi
