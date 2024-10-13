697e510045b79f0f2985b255893e24990a8f75b6
khangtictoc <tranhoangkhang09112001@gmail.com>
2024-10-13 21:02:52 +0700

    Add automation config oh-my-posh for zsh and fish

--- linux/terminal/ui/oh-my-posh/configure-fish.sh
+++ linux/terminal/ui/oh-my-posh/configure-fish.sh
@@ -8,4 +8,13 @@ set THEME "easy-term"
 set APPLIED_SHELL "fish"
 oh-my-posh init $APPLIED_SHELL --config ~/.cache/oh-my-posh/themes/$THEME.omp.json > ~/.oh-my-posh-init
 source ~/.oh-my-posh-init
-echo -n "[ -f ~/.oh-my-posh-init ] && source ~/.oh-my-posh-init" | tee -a ~/.config/fish/config.fish
+
+# Config for fish at statup with fish
+if grep -Fxq "[ -f ~/.oh-my-posh-init ] && source ~/.oh-my-posh-init" ~/.config/fish/config.fish
+    echo "Fish file config has already source the ~/.oh-my-posh-init at fish startup. NO MODIFY !!!"
+else
+    echo -n "[ -f ~/.oh-my-posh-init ] && source ~/.oh-my-posh-init" | tee -a ~/.config/fish/config.fish
+    echo "\nFish file config will source the ~/.oh-my-posh-init at fish startup". MODIFY SUCCESSFULLY !!!
+end
+
+
