0413adda2950be8c90370be012a489f22470b32e
khangtictoc <tranhoangkhang09112001@gmail.com>
2024-10-13 21:05:56 +0700

    Add bash start with fish

--- linux/terminal/shell/fish/install.sh
+++ linux/terminal/shell/fish/install.sh
@@ -3,4 +3,10 @@
 sudo apt-add-repository -y ppa:fish-shell/release-3
 sudo apt update
 sudo apt install fish -y
-echo "fish" | tee -a $HOME/.bashrc
+
+if grep -Fxq "[ -f ~/.oh-my-posh-init ] && source ~/.oh-my-posh-init" $HOME/.bashrc
+    echo "Fish command has already been at bash startup. NO MODIFY !!!"
+else
+    echo "fish" | tee -a $HOME/.bashrc
+    echo "\nFish command at bash startup". MODIFY SUCCESSFULLY !!!
+end
