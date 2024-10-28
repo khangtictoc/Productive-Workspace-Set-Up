78e24b695217d2937d6026edd40fe2814217cc82
khangtictoc <tranhoangkhang09112001@gmail.com>
2024-10-29 02:09:01 +0700

    Add startup for fish

--- linux/start/all-in-one.sh
+++ linux/start/all-in-one.sh
@@ -1,15 +1,18 @@
 #! /usr/bin/fish
 
+# Array of URLs
+set -l urls "https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/alias/advance_function/.utilities_fish" "https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/alias/command_extension/.extension_aliases" "https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/alias/git/.git_aliases_fish" "https://raw.githubusercontent.com/khangatmercatus/Productive-Workspace-Set-Up/refs/heads/main/linux/alias/kubernetes/helm-aliases/.helm_aliases" "https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/alias/kubernetes/kubectl-aliases/.kubectl_aliases" "https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/alias/system-aliases/.system_aliases"
+
 set DOTFILES_DIRNAME dotfiles
+mkdir -p ~/$DOTFILES_DIRNAME
 
-mkdir ~/$DOTFILES_DIRNAME
+for i in (seq 6)
+    set filename (basename "$urls[$i]")
+    wget $urls[$i] -O ~/$DOTFILES_DIRNAME/$filename
+end
 
-wget https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/alias/advance_function/.utilities_fish -P ~/$DOTFILES_DIRNAME
-wget https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/alias/command_extension/.extension_aliases -P ~/$DOTFILES_DIRNAME
-wget https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/alias/git/.git_aliases -P ~/$DOTFILES_DIRNAME
-wget https://raw.githubusercontent.com/khangatmercatus/Productive-Workspace-Set-Up/refs/heads/main/linux/alias/kubernetes/helm-aliases/.helm_aliases -P ~/$DOTFILES_DIRNAME
-wget https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/alias/kubernetes/kubectl-aliases/.kubectl_aliases -P ~/$DOTFILES_DIRNAME
-wget https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/alias/system-aliases/.system_aliases -P ~/$DOTFILES_DIRNAME
-
-
-find ~/$DOTFILES_DIRNAME -type f | xargs -I {} bash -c "source {}"
+for file in ~/$DOTFILES_DIRNAME/.*
+    if test -r $file
+        source $file
+    end
+end
