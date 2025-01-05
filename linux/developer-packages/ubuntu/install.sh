f294f8c7315452defcc342450541215d20b60955
khangtictoc <tranhoangkhang09112001@gmail.com>
2025-01-05 21:22:50 +0700

    Add command for network cheatsheet and developer packages

--- linux/developer-packages/ubuntu/install.sh
+++ linux/developer-packages/ubuntu/install.sh
@@ -1,3 +1,7 @@
+## ENVIRONMENT VARIABLES ##
+set HELM_VERSION v3.16.4
+HELM_VERSION=v3.16.4
+
 ## GENERAL ##
 apt install -y unzip
 # File editor
@@ -41,6 +45,7 @@ apt install -y lshw # Display hardware info
 apt install -y tilix # Add multi-terminal in 1 display
 apt install -y rg # ripgrep - Search content in multiples files or folders
 apt install -y bc # Convenient calculator
+apt install -y jq # JSON Values Extractor
 
 # Database
 ## MySQL
@@ -59,3 +64,22 @@ sudo mv ./kubectl-node_shell /usr/local/bin/kubectl-node_shell
 # Superfile - File Manager
 bash -c "$(curl -sLo- https://superfile.netlify.app/install.sh)"
 
+# Helm
+curl -O https://get.helm.sh/helm-$HELM_VERSION-linux-amd64.tar.gz
+tar -zxvf helm-$HELM_VERSION-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
+
+# AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Terraform
+wget https://releases.hashicorp.com/terraform/1.10.3/terraform_1.10.3_linux_amd64.zip
+unzip terraform_1.10.3_linux_amd64.zip
+sudo mv terraform /usr/local/bin/terraform
+
+# Terragrunt
+wget https://github.com/gruntwork-io/terragrunt/releases/download/v0.71.2-alpha2024122002/terragrunt_linux_amd64
+sudo chmod u+x terragrunt_linux_amd64
+sudo mv terragrunt_linux_amd64 /usr/local/bin/terragrunt
