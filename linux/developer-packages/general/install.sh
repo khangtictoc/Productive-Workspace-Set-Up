9edd13b912f12b921f2d56f8687459148cc50313
Khang Tran <khang.tran@mercatus.com>
2025-05-07 22:23:14 +0700

    Add kubectl command & modify utility for k8s

--- linux/developer-packages/general/install.sh
+++ linux/developer-packages/general/install.sh
@@ -14,3 +14,13 @@ begin
 end
 
 set -gx PATH $PATH $HOME/.krew/bin
+
+
+# Detect secrets
+echo "==== INSTALLING DETECT SECRETS ===="
+wget "https://codeload.github.com/Yelp/detect-secrets/zip/refs/tags/v1.5.0" -O detect-secrets.zip
+unzip detect-secrets.zip
+cd detect-secrets-1.5.0
+python3 setup.py install
+cd .. && rm -rf detect-secrets-1.5.0 detect-secrets.zip
+echo "==== CLEAN UP ===="
--- linux/developer-packages/general/python-pip-packages.sh
+++
@@ -1,9 +0,0 @@
-# Detect secrets
-echo "==== INSTALLING DETECT SECRETS ===="
-wget "https://codeload.github.com/Yelp/detect-secrets/zip/refs/tags/v1.5.0" -O detect-secrets.zip
-unzip detect-secrets.zip
-cd detect-secrets-1.5.0
-python3 setup.py install
-cd .. && rm -rf detect-secrets-1.5.0 detect-secrets.zip
-echo "==== CLEAN UP ===="
-
