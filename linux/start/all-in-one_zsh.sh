1a2d2d92abf75d3c87e077fc1609accf915fcd2e
Khang Tran <tranhoangkhang09112001@gmail.com>
2025-05-08 21:34:50 +0700

    Push pre-push git hook script

--- linux/start/all-in-one_zsh.sh
+++ linux/start/all-in-one_zsh.sh
@@ -57,6 +57,7 @@ sleep 1
 echo "Configuring Global Client-side Git Hook ..."
 mkdir -p ~/$GITCONFIG_DIRNAME/hooks
 git config --global core.hooksPath ~/$GITCONFIG_DIRNAME/hooks
+wget -q "https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/" -O ~/$GITCONFIG_DIRNAME/alias/git_aliases.txt
 
 ## SET UP ZSH PROFILES
 echo "============ SET UP ZSH PROFILES ============"
