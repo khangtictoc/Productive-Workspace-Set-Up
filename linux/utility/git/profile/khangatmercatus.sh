26cc6012066aa52376a27e987fe5e36f9e84fd56
Khang Tran <khang.tran@mercatus.com>
2025-05-19 17:45:51 +0700

    Change git profile retrieval method

---
+++ linux/utility/git/profile/khangatmercatus.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+git config --global user.name "Khang Tran"
+git config --global user.email "khang.tran@mercatus.com"
+git config --global user.username "khangatmercatus"
---
+++ linux/utility/git/profile/khangtictoc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git config --global user.name "Khang Hoang Tran"
+git config --global user.email "tranhoangkhang09112001@gmail.com"
+git config --global user.username "khangtictoc"
