383c46ceb75958f5069e47e15886dc326cfa5434
Khang Hoang Tran <tranhoangkhang09112001@gmail.com>
2025-08-09 12:22:07 +0700

    [window] feat: Add customize settings of zebar,windhawk for new computer setup

---
+++ windows/installation/status_bar/zebar/README.md
@@ -0,0 +1,3 @@
+# Config
+
+https://github.com/khangtictoc/zebar_neon_theme
---
+++ windows/start/startup_workspace.bat
@@ -0,0 +1,20 @@
+@echo off
+REM ====== Launch all workspace apps ======
+
+REM Launch Microsoft Edge
+start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+
+REM Launch Visual Studio Code
+start "" "D:\KHANG\Software\Microsoft VS Code\Code.exe"
+
+REM Launch 1Password
+start "" "C:\Users\tranh\AppData\Local\1Password\app\8\1Password.exe"
+
+REM Launch Thunderbird
+start "" "D:\KHANG\Software\Thunderbird\thunderbird.exe"
+
+REM Launch Zalo
+start "" "C:\Users\tranh\AppData\Local\Programs\Zalo\Zalo.exe"
+
+REM Done
+exit
