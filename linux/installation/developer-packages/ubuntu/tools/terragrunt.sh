af6586939de939ed1f8ec3af161f02938e0ae423
Khang Hoang Tran <tranhoangkhang09112001@gmail.com>
2025-09-15 01:52:56 +0700

    update: terragrunt installation script

--- linux/installation/developer-packages/ubuntu/tools/terragrunt.sh
+++ linux/installation/developer-packages/ubuntu/tools/terragrunt.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 
 TFG_VERSION="0.71.2-alpha2024122002"
-wget https://github.com/gruntwork-io/terragrunt/releases/download/v${TFG_VERSION}/terragrunt_linux_amd64
-sudo chmod u+x terragrunt_linux_amd64
-sudo mv terragrunt_linux_amd64 /usr/local/bin/terragrunt
+
+if ! command -v terragrunt 2>&1 >/dev/null
+then
+    echo "[INSTALLING ⬇️ ] Terragrunt"
+    wget https://github.com/gruntwork-io/terragrunt/releases/download/v${TFG_VERSION}/terragrunt_linux_amd64
+    sudo chmod u+x terragrunt_linux_amd64
+    sudo mv terragrunt_linux_amd64 /usr/local/bin/terragrunt
+else
+    echo "- [CHECKED ✅] terragrunt command exists"
+fi
+
