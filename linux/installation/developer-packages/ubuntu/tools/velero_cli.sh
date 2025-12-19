172b89d8cb061796a916f262e345ecb0a57e22ed
Khang Hoang Tran <tranhoangkhang09112001@gmail.com>
2025-12-19 23:27:38 +0700

    feat: Add velero installation script

---
+++ linux/installation/developer-packages/ubuntu/tools/velero_cli.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+VERSION="v1.17.0"
+
+if ! command -v velero 2>&1 >/dev/null
+then
+    wget --progress=dot:giga "https://github.com/vmware-tanzu/velero/releases/download/$VERSION/velero-$VERSION-linux-amd64.tar.gz"
+    tar xfvz velero-$VERSION-linux-amd64.tar.gz
+    cp velero-$VERSION-linux-amd64/velero /usr/local/bin
+
+    echo "[INFO] >>>> Clean Up"
+    rm -drf velero-$VERSION-linux-amd64.tar.gz velero-$VERSION-linux-amd64
+
+    if ! command -v velero &> /dev/null; then
+        echo "[FAIL ❌] velero installation failed!"
+        exit 1
+    fi
+
+    echo "- [CHECKED ✅] velero command installed!"
+else
+    echo "- [CHECKED ✅] velero command exists!"
+fi
