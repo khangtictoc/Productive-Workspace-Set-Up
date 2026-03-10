3577834fac7f245f502fa138585742800db778d1
Khang Hoang Tran <tranhoangkhang09112001@gmail.com>
2026-03-10 22:25:20 +0700

    [macos,linux] feat: startup script

--- linux/installation/developer-packages/ubuntu/tools/ansible.sh
+++ linux/installation/developer-packages/ubuntu/tools/ansible.sh
@@ -4,7 +4,7 @@ ANSIBLE_VERSION="6.7.0"
 
 # Note: Remember to "export SHELLRC_FILE", e.g. '$HOME/.zshrc' or '$HOME/.bashrc'
 
-if ! command -v ansible &>/dev/null; then
+if ! ansible -h &>/dev/null; then
     pip3 install ansible==$ANSIBLE_VERSION
 
     # Add "$HOME/.local/bin" to PATH
@@ -15,7 +15,9 @@ if ! command -v ansible &>/dev/null; then
         echo "[INFO] Existed: \$HOME/.local/bin already in PATH"
     fi
 
-    if ! command -v ansible &>/dev/null; then
+    export PATH="$HOME/.local/bin:$PATH"
+
+    if ansible -h &>/dev/null; then
         echo "[FAIL ❌] ansible installation failed!"
         exit 1
     fi
