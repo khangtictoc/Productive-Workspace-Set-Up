e8e0ddd6f34c14938d5c568e6329524da1d25d6d
Khang Hoang Tran <tranhoangkhang09112001@gmail.com>
2026-07-27 22:02:46 +0700

    modify personal script

---
+++ linux/start/personal-setup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+git clone https://github.com/khangtictoc/Monitoring--GrafanaStack.git "1.Monitoring GrafanaStack"
+git clone https://github.com/khangtictoc/Productive-Workspace-Set-Up.git "2.1 Productive Workspace"
+git clone https://github.com/khangtictoc/DevOps-Tools-Installation-Scripts.git "2.2 DevOps Tools"
+git clone https://github.com/khangtictoc/Terragrunt_Project_Structure_Design.git "3. Terragrunt Project"
+git clone https://github.com/khangtictoc/khangtictoc.github.io.git "4. My Blog"
+git clone https://github.com/khangtictoc/ArgoCD-Apps.git "5. ArgoCD Apps"
+git clone https://github.com/khangtictoc/myportfolio.git "6. My Portfolio"
+git clone https://github.com/khangtictoc/All-In-One-Devops-Pipelines.git "7.1 Devops Pipelines"
+git clone https://github.com/khangtictoc/Personal-Pipelines.git "7.2 Personal Pipelines"
+git clone https://github.com/khangtictoc/Jenkins-Library "10. Jenkins Library"
+git clone https://github.com/khangtictoc/POC-All-In-One-Azure.git "12. POC"
